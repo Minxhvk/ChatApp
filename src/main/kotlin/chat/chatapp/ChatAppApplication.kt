7dcd1adae4b7b26b66ff8424e3b29941f851bc3d
Minxhvk <minxhvk@gmail.com>
2023-05-13 17:35:32 +0900

    first commit

---
+++ src/main/kotlin/chat/chatapp/ChatAppApplication.kt
@@ -0,0 +1,21 @@
+package chat.chatapp
+
+import mu.KotlinLogging
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+private val logger = KotlinLogging.logger {}
+
+@SpringBootApplication
+class ChatAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatAppApplication>(*args)
+
+    // Logging
+    logger.trace { "I'm trace! hello, world." }
+    logger.debug { "I'm debug! Hello world." }
+    logger.info { "I'm info! Hello world." }
+    logger.warn { "I'm warn! Hello world." }
+    logger.error { "I'm error! Hello world." }
+}
